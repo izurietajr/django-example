f5c6d387c2dcdc100b4aacde378f06d3e4764797
Jesus Izurieta <izurietajr@hotmail.com>
2019-05-17 10:51:11 -0400

    models created

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Entity(models.Model):
+
+    name = models.CharField(
+        verbose_name='Nombre',
+        max_length=100,
+        null=False, blank=False
+    )
+
+    description = models.CharField(
+        verbose_name='Descripción',
+        max_length=500,
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+
+    text = models.CharField(
+        verbose_name="Comentario",
+        max_length=500,
+        null=False, blank=False
+    )
+
+    date = models.DateTimeField(
+        verbose_name="Fecha",
+        null=True, blank=True
+    )
+
+    entity = models.ForeignKey(
+        to=Entity,
+        related_name="comments",
+        verbose_name="Entidad",
+        null=False, blank=False,
+        on_delete=models.CASCADE,
+    )
+
+
+class Score(models.Model):
+
+    punctuation = models.IntegerField(
+        verbose_name="Puntuación",
+        null=False, blank=True
+    )
+
+    entity = models.ForeignKey(
+        to=Entity,
+        related_name="score_list",
+        verbose_name="Entidad",
+        null=False, blank=False,
+        on_delete=models.CASCADE,
+    )
